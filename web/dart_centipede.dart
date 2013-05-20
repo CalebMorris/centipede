a05f3213504a2e99da452470eb7c7457ebd0c9ff
Caleb Morris <caleb.morris.g@gmail.com>
2013-05-19 23:19:12 -0700

    license

--- web/dart_centipede.dart
+++ web/dart_centipede.dart
@@ -2,12 +2,23 @@ import 'dart:html';
 import 'dart:math';
 import 'dart:async';
 
-/*      
-        Centipeed
-  Simple game that you collect food and grow until there is no more room.
-  End Condition: All body blocks have two adjacent blocks
-  Bodysection size = 8px
-  Border = 4px
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+Author: Caleb Morris <Caleb.Morris.G@gmail.com>
+
+Centipede
 */
 
 void main() {
